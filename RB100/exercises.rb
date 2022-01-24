11f48329fea82d71cc729186e782b73b938a89a2
raphaelcosta93 <raphaelcosta151@yahoo.com>
2022-01-24 02:25:04 -0700

    Done with Ruby Exercises

--- RB100/exercises.rb
+++ RB100/exercises.rb
@@ -1,3 +1,20 @@
-a = ['white snow', 'winter wonderland', 'melting ice',
-  'slippery sidewalk', 'salted roads', 'white trees']
-a = a.map { |pairs| pairs.split }
+# Each player starts with the same basic stats.
+
+player = { strength: 10, dexterity: 10, charisma: 10, stamina: 10 }
+
+# Then she picks a character class and gets an upgrade accordingly.
+
+character_classes = {
+  warrior: { strength:  20 },
+  thief:   { dexterity: 20 },
+  scout:   { stamina:   20 },
+  mage:    { charisma:  20 }
+}
+
+puts 'Please type your class (warrior, thief, scout, mage):'
+input = gets.chomp.downcase
+
+player = player.merge(character_classes[input.to_sym])
+
+puts 'Your character stats:'
+puts player
---
+++ RB100/something.rb
@@ -0,0 +1,2 @@
+puts '>> Do you want me to print something? (y/n)'
+choice = gets.chomp
