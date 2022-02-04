a6b2d819e77b980851fbca71689dfc3d9f4fc0f6
raphaelcosta93 <raphaelcosta151@yahoo.com>
2022-02-03 23:40:32 -0700

    commit

--- RB101/exercises.rb
+++ RB101/exercises.rb
@@ -1,3 +1,18 @@
-test test
 
-whatever you we are testing git pull and push functions
+def car(new_car)
+  make = make(new_car)
+  model = model(new_car)
+  [make, model]
+end
+
+def make(new_car)
+  new_car.split(" ")[0]
+end
+
+def model(new_car)
+  new_car.split(" ")[1]
+end
+
+make, model = car("Ford Mustang")
+make == "Ford"         # => true
+model.start_with?("M") # => NoMethodError: undefined method `start_with?' for nil:NilClass
